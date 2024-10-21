e2f4f4227af62698aca91cdf1df7546e3efbb9ca
Dmytro <dolgiy.dmitriyp@gmail.com>
2024-10-21 19:16:49 +0200

    Minor changes

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,15 +1,15 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-  info: {
-    title: 'My API',
-    description: 'Description'
-  },
-  host: 'localhost:3000'
+    info: {
+        title: 'My API',
+        description: 'Description'
+    },
+    host: 'localhost:3000'
 };
 
 const outputFile = '../../swagger-output.json';
-const routes = ['./src/app.js'];
+const routes = ['../app.js'];
 
 
 swaggerAutogen(outputFile, routes, doc);
